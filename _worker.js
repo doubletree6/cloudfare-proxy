de26bb78d96f5d690f3dbe790f6135b31c0da9de
doubletree6 <108912608+doubletree6@users.noreply.github.com>
2023-09-06 11:54:00 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,27 +1,32 @@
 const TELEGRAPH_URL = 'https://huggingface.co/spaces/qinglin96/chatgpt-academic-freeai';
 
-
 export default {
   async fetch(request, env) {
-      const NewResponse = await handleRequest(request)
-      return NewResponse
+    const NewResponse = await handleRequest(request);
+    return NewResponse;
   },
-
 };
 
 async function handleRequest(request) {
   const url = new URL(request.url);
-  const headers_Origin = request.headers.get("Access-Control-Allow-Origin") || "*"
-  url.host = TELEGRAPH_URL.replace(/^https?:\/\//, '');
+  const headers_Origin =
+    request.headers.get('Access-Control-Allow-Origin') || '*';
+
+  // 将整个 TELEGRAPH_URL 替换掉传入请求的 URL
+  url.href = TELEGRAPH_URL;
+
   const modifiedRequest = new Request(url.toString(), {
     headers: request.headers,
     method: request.method,
     body: request.body,
-    redirect: 'follow'
+    redirect: 'follow',
   });
+
   const response = await fetch(modifiedRequest);
   const modifiedResponse = new Response(response.body, response);
+
   // 添加允许跨域访问的响应头
   modifiedResponse.headers.set('Access-Control-Allow-Origin', headers_Origin);
+
   return modifiedResponse;
 }
